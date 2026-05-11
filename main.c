835541892630119cdaaa3c60e466cdb0765e68c5
Kelvin Phillipe <kellipe234@gmail.com>
2026-05-10 22:17:58 -0300

    Update print statements in main.c

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("Aluno editando V2.\n");
+    printf("Aluno editando V1.\n");
+    printf("Aluno editando v2\n");
     printf("Aluno editando v3\n");
+    printf("Aluno editando v4\n");
     
     return 0;
 }
